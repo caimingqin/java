5b981d78a57e5110790e13cf6192940a9ba3e8db
cai.mingqin <737895494@qq.com>
2012-12-19 16:06:48 +0800

    Signed-off-by: cai.mingqin <737895494@qq.com>

--- src/com/domain/Hello.java
+++ src/com/domain/Hello.java
@@ -3,7 +3,9 @@ package com.domain;
 public class Hello {
 
 	private String code;
+	private String age;
     private String name;
+    private String aaa;
 	public String getCode() {
 		return code;
 	}
